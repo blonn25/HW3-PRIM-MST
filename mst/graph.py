91c73d4b3a7663b4640af0d30dafb91d74fd222e
blonn25 <beaulonnquist@gmail.com>
2026-01-23 17:19:49 -0800

    a bit of work on Primm's algorithm - untested for now, but it's a start :)

--- mst/graph.py
+++ mst/graph.py
@@ -41,4 +41,65 @@ class Graph:
         `heapify`, `heappop`, and `heappush` functions.
 
         """
-        self.mst = None
+
+        # check if the tree is fully connected; if not, throw an error
+        # this will happen if any node has no connections row/col sums to 0
+        if np.any(self.adj_mat.sum(axis=0) == 0):
+            raise ValueError("This graph is not fully connected. No minimum spanning tree exists.")
+
+        # init arbitrary starting node s, a set of explored nodes, and the MST
+        s = 0                                       # start exploring from node 0
+        explored = {}                               # set of visited explored nodes (just s for now)
+        mst = np.zeros_like(self.adj_mat)           # the current mst
+
+        # init list min_cost to track the cheapest known edge between node v and explored nodes.
+        # (e.g. min_cost[2] is the cheapest known edge from node 2 to any of the explored nodes)
+        min_cost = [float('inf')] * self.adj_mat.shape[0]
+        min_cost[s] = 0
+
+        # init list pred, which keeps track of each node to the current MST
+        # (e.g. pred[2] is the predecessor of node 2 in the MST)
+        pred = [None] * self.adj_mat.shape[0]
+
+        # init an empty min priority queue pq and push all values into pq ordered by their
+        # cheapest known edge to the explored nodes
+        pq = [(min_cost[v], v) for v in range(self.adj_mat.shape[0])]
+        heapq.heapify(pq)
+
+        # while the priority queue is not empty, continue to grow the tree
+        while len(pq) > 0:
+            
+            # pop the cheapest node
+            cost, u = heapq.heappop(pq)     
+
+            # if node u has already been explored, continue (no need to even consider)
+            if u in explored:
+                continue
+
+            # add node u to the explored list
+            explored.append(u)
+
+            # as long as this is not the first iteration, update the edges in the mst
+            if pred != None:
+                mst[u, pred[u]] = cost          # connect u to its predecessor already in the mst
+                mst[pred[u], u] = cost          # also update symmetric edge in adj matrix
+
+            # look at each node connected to u
+            u_neighbors = np.nonzero(self.adj_mat[u, :])[0]
+            for v in u_neighbors:
+
+                # if node v has already been explored, continue (no need to even consider)
+                if v in explored:
+                    continue
+                
+                # get the edge cost from u to v
+                edge_cost = self.adj_mat[u, v]
+
+                # if the cost from u to v is less than the current cost to add v to the tree,
+                # update min_cost and pred accordingly
+                if edge_cost < min_cost[v]:
+                    min_cost[v] = edge_cost     # updates 
+                    pred[v] = u
+                    heapq.heappush(pq, (edge_cost, v))
+
+        self.mst = mst
